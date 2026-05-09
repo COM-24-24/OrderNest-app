4d2e7c22224f55c131adc609f9399127c9d0aaeb
bsc-inf-12-24 <bsc-inf-12-24@unima.ac.mw>
2026-05-09 13:20:03 +0200

    Monitor: has removed fooditem

--- src/fooditems/dto/update-fooditem.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types'; 
-import { CreateFooditemDto } from './create-fooditem.dto';
-import { ApiExtraModels, ApiPropertyOptional } from '@nestjs/swagger';
-
-@ApiExtraModels(CreateFooditemDto)
-export class UpdateFooditemDto extends PartialType(CreateFooditemDto) {}
